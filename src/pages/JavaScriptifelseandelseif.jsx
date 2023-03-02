eb11a30da51034dfce6b211d400e79bcc5f44cc8
GoulartAugusto <goulart.augusto@gmail.com>
2023-03-02 03:46:15 -0300

    add: JavaScript 'if else' description page

--- src/pages/JavaScriptifelseandelseif.jsx
+++ src/pages/JavaScriptifelseandelseif.jsx
@@ -48,9 +48,221 @@ const JavaScriptifelseandelseif = () => {
 
         <p>Use the <code style={{color:'blue'}}>if</code> statement to specify a block of JavaScript code to be executed if a condition is true.</p>
 
+        <br />
 
+        <h4>Syntax</h4>
 
+        <div style={{background:'#cecece'}}>
+            <code>
+                <code style={{color:'blue'}}>if</code> (condition) &#123;
 
+                <br />
+
+                <code style={{color:'green'}}>// block of code to be executed if the condition is true</code>
+
+                <br />
+
+                &#125;
+            </code>
+        </div>
+
+        <br />
+
+        <div style={{background:'lightyellow'}}>
+            <br />
+
+            <p>Note that <code style={{color:'blue'}}>if</code> is in lowercase letters. Uppercase letters (If or IF) will generate JavaScript error.</p>
+
+            <br />
+        </div>
+
+        <br />
+        
+        <div style={{background:'#cecece'}}>
+            <h3>Example</h3>
+
+            <p>Make a "Good day" greeting if the hour is less than 18:00:</p>
+
+            <code>
+                <code style={{color:'blue'}}>if</code> (hour &lt; <code style={{color:'red'}}>18</code>) &#123;
+
+                <br />
+
+                <code> greeting = <code style={{color:'#5d0000'}}>"Good day"</code>;</code>
+
+                <br />
+
+                &#125;
+            </code>
+
+            <br />
+
+            <p>The result of greeting will be:</p>
+
+            <p><code>Good day</code></p>
+        </div>
+
+        <br />
+
+        {/* Just a mark to separete sections */}
+
+        <h2>The else Statement</h2>
+
+        <p>Use the <code style={{color:'blue'}}>else</code> statement to specify a block of code to be executed if the condition is false.</p>
+
+        <br />
+
+        <div style={{background:'#cecece'}}>
+            <code>
+                <code style={{color:'blue'}}>if</code> (condition) &#123;
+
+                <br />
+
+                <code style={{color:'green'}}>// block of code to be executed if the condition is true</code>
+
+                <br />
+
+                <code>
+                    &#125; <code style={{color:'blue'}}>else</code> &#123;
+                </code>
+
+                <br />
+
+                <code style={{color:'green'}}>//  block of code to be executed if the condition is false</code>
+
+                <br />
+
+                &#125;
+            </code>
+        </div>
+
+        <br />
+
+        <div style={{background:'#cecece'}}>
+            <h3>Example</h3>
+
+            <p>If the hour is less than 18, create a "Good day" greeting, otherwise "Good evening":</p>
+
+            <code>
+                <code style={{color:'blue'}}>if</code> (hour &lt; <code style={{color:'red'}}>18</code>) &#123;
+
+                <br />
+
+                <code> greeting = <code style={{color:'#5d0000'}}>"Good day"</code>;</code>
+
+                <br />
+
+                <code>
+                    &#125; <code style={{color:'blue'}}>else</code> &#123;
+                </code>
+
+                <br />
+
+                <code> greeting = <code style={{color:'#5d0000'}}>"Good evening"</code>;</code>
+
+                <br />
+
+                &#125;
+            </code>
+
+            <br />
+
+            <p>The result of greeting will be:</p>
+
+            <p><code>Good day</code></p>
+        </div>
+
+        <br />
+
+        {/* Just a mark to separete sections */}
+
+        <h2>The else if Statement</h2>
+
+        <p>Use the <code style={{color:'blue'}}>else if</code> statement to specify a new condition if the first condition is false.</p>
+
+        <br />
+
+        <div style={{background:'#cecece'}}>
+            <code>
+                <code style={{color:'blue'}}>if</code> (condition1) &#123;
+
+                <br />
+
+                <code style={{color:'green'}}>// block of code to be executed if the condition1 is true</code>
+
+                <br />
+
+                <code>
+                    &#125; <code style={{color:'blue'}}>else if</code> (condition2) &#123;
+                </code>
+
+                <br />
+
+                <code style={{color:'green'}}>// block of code to be executed if the condition1 is false and condition2 is true</code>
+
+                <br />
+
+                <code>
+                    &#125; <code style={{color:'blue'}}>else</code> &#123;
+                </code>
+
+                <br />
+
+                <code style={{color:'green'}}>// block of code to be executed if the condition1 is false and condition2 is false</code>
+
+                <br />
+
+                &#125;
+            </code>
+        </div>
+
+        <br />
+
+        <br />
+
+        <div style={{background:'#cecece'}}>
+            <h3>Example</h3>
+
+            <p>If time is less than 10:00, create a "Good morning" greeting, if not, but time is less than 20:00, create a "Good day" greeting, otherwise a "Good evening":</p>
+
+            <code>
+                <code style={{color:'blue'}}>if</code> (time &lt; <code style={{color:'red'}}>10</code>) &#123;
+
+                <br />
+
+                <code> greeting = <code style={{color:'#5d0000'}}>"Good morning"</code>;</code>
+
+                <br />
+
+                <code>
+                    &#125; <code style={{color:'blue'}}>else if</code> (time &lt; <code style={{color:'red'}}>20</code>) &#123;
+                </code>
+
+                <br />
+
+                <code> greeting = <code style={{color:'#5d0000'}}>"Good day"</code>;</code>
+
+                <br />
+
+                <code>
+                    &#125; <code style={{color:'blue'}}>else</code> &#123;
+                </code>
+
+                <br />
+
+                <code> greeting = <code style={{color:'#5d0000'}}>"Good evening"</code>;</code>
+
+                <br />
+
+                &#125;
+            </code>
+
+            <br />
+
+            <p>The result of greeting will be:</p>
+
+            <p><code>Good morning</code></p>
+        </div>
 
     </main>
   )
