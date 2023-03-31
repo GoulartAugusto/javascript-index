a49484716eea8e9f39c123b05fb8d33367555971
GoulartAugusto <goulart.augusto@gmail.com>
2023-03-31 18:15:39 -0300

    describing javascript bitwise

--- src/pages/JavaScriptBitwiseOperations.jsx
+++ src/pages/JavaScriptBitwiseOperations.jsx
@@ -150,6 +150,276 @@ const JavaScriptBitwiseOperations = () => {
         {/* Just a mark to separete sections */}
 
         <h2>JavaScript Bitwise AND</h2>
+
+        <p>When a bitwise AND is performed on a pair of bits, it returns 1 if both bits are 1.</p>
+
+        <p>One bit example:</p>
+
+        <table>
+            <tr>
+                <th>Operation</th>
+                <th>Result</th>
+            </tr>
+            <tr>
+                <td>0 & 0</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>0 & 1</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>1 & 0</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>1 & 1</td>
+                <td>1</td>
+            </tr>
+        </table>
+
+        <br />
+
+        <p>4 bits example:</p>
+        
+        <table>
+            <tr>
+                <th>Operation</th>
+                <th>Result</th>
+            </tr>
+            <tr>
+                <td>1111 & 0000</td>
+                <td>0000</td>
+            </tr>
+            <tr>
+                <td>1111 & 0001</td>
+                <td>0001</td>
+            </tr>
+            <tr>
+                <td>1111 & 0010</td>
+                <td>0010</td>
+            </tr>
+            <tr>
+                <td>1111 & 0100</td>
+                <td>0100</td>
+            </tr>
+        </table>
+
+        <br />
+
+        {/* Just a mark to separete sections */}
+
+        <h2>JavaScript Bitwise OR</h2>
+
+        <p>When a bitwise OR is performed on a pair of bits, it returns 1 if one of the bits is 1:</p>
+
+        <p>One bit example:</p>
+
+        <table>
+            <tr>
+                <th>Operation</th>
+                <th>Result</th>
+            </tr>
+            <tr>
+                <td>0 | 0</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>0 | 1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1 | 0</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1 | 1</td>
+                <td>1</td>
+            </tr>
+        </table>
+
+        <br />
+
+        <p>4 bits example:</p>
+        
+        <table>
+            <tr>
+                <th>Operation</th>
+                <th>Result</th>
+            </tr>
+            <tr>
+                <td>1111 | 0000</td>
+                <td>1111</td>
+            </tr>
+            <tr>
+                <td>1111 | 0001</td>
+                <td>1111</td>
+            </tr>
+            <tr>
+                <td>1111 | 0010</td>
+                <td>1111</td>
+            </tr>
+            <tr>
+                <td>1111 | 0100</td>
+                <td>1111</td>
+            </tr>
+        </table>
+
+        <br />
+
+        {/* Just a mark to separete sections */}
+
+        <h2>JavaScript Bitwise XOR</h2>
+
+        <p>When a bitwise XOR is performed on a pair of bits, it returns 1 if the bits are different:</p>
+
+        <p>One bit example:</p>
+
+        <table>
+            <tr>
+                <th>Operation</th>
+                <th>Result</th>
+            </tr>
+            <tr>
+                <td>0 ^ 0</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>0 ^ 1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1 ^ 0</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1 ^ 1</td>
+                <td>0</td>
+            </tr>
+        </table>
+
+        <br />
+
+        <p>4 bits example:</p>
+
+        <table>
+            <tr>
+                <th>Operation</th>
+                <th>Result</th>
+            </tr>
+            <tr>
+                <td>1111 ^ 0000</td>
+                <td>1111</td>
+            </tr>
+            <tr>
+                <td>1111 ^ 0001</td>
+                <td>1110</td>
+            </tr>
+            <tr>
+                <td>1111 ^ 0010</td>
+                <td>1101</td>
+            </tr>
+            <tr>
+                <td>1111 ^ 0100</td>
+                <td>1011</td>
+            </tr>
+        </table>
+
+        <br />
+
+        {/* Just a mark to separete sections */}
+
+        <h2>JavaScript Bitwise AND (&)</h2>
+
+        <p>Bitwise AND returns 1 only if both bits are 1:</p>
+
+        <table>
+            <tr>
+                <th>Decimal</th>
+                <th>Binary</th>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>00000000000000000000000000000101</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>00000000000000000000000000000001</td>
+            </tr>
+            <tr>
+                <td>5 & 1</td>
+                <td>00000000000000000000000000000001 (1)</td>
+            </tr>
+        </table>
+
+        <br />
+
+        <div style={{background:'#cecece'}}>
+            <h3>Example</h3>
+
+            <code>
+                <code style={{color:'blue'}}>let</code> x = <code style={{color:'red'}}>5</code> & <code style={{color:'red'}}>1</code>;
+            </code>
+
+            <br />
+
+            <p>The & Operator</p>
+
+            <p>return</p>
+
+            <code>1</code>
+        </div>
+
+        <br />
+
+        {/* Just a mark to separete sections */}
+
+        <h2>JavaScript Bitwise OR (|)</h2>
+
+        <p>Bitwise OR returns 1 if one of the bits is 1:</p>
+
+        <table>
+            <tr>
+                <th>Decimal</th>
+                <th>Binary</th>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>00000000000000000000000000000101</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>00000000000000000000000000000001</td>
+            </tr>
+            <tr>
+                <td>5 | 1</td>
+                <td>00000000000000000000000000000001 (5)</td>
+            </tr>
+        </table>
+
+        <br />
+
+        <div style={{background:'#cecece'}}>
+            <h3>Example</h3>
+
+            <code>
+                <code style={{color:'blue'}}>let</code> x = <code style={{color:'red'}}>5</code> | <code style={{color:'red'}}>1</code>;
+            </code>
+
+            <br />
+
+            <p>The | Operator</p>
+
+            <p>return</p>
+
+            <code>5</code>
+        </div>
+
+        <br />
+
+        {/* Just a mark to separete sections */}
+        
+        <h2></h2>
     </main>
   )
 }
